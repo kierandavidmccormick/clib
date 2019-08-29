5c1a5833b341770adbcf7d571451dc640836781f
Kieran McCormick <kieran.david.mccormick@gmail.com>
2019-08-29 13:40:41 -0500

    began testing of linked list

--- Test.c
+++ Test.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-void bubbleSortGen(void *list[], int size, int(*comparator)(void *, void *));
-int compareInt(int *a, int *b);
-void swap(void **a, void **b);
-void printList(void *list[]);
-
 typedef struct linkedListElement {
-	struct ll_element *next;
+	struct linkedListElement *next;
 	void *value;
 } ll_element;
 
 typedef struct linkedListHead {
 	int size;
-	struct ll_element *first;
-	struct ll_element *last;
+	struct linkedListElement *first;
+	struct linkedListElement *last;
+	void (*dealloc)(void *);
 } ll_head;
 
+//TODO: make things that need to be static or private static or private
+
+void bubbleSortGen(void *list[], int size, int(*comparator)(void *, void *));
+int compareInt(int *a, int *b);
+void swap(void **a, void **b);
+void printList(void *list[]);
+ll_head *ll_create(void (*dealloc)(void *));
+ll_element *ll_getIndex(int index, ll_head *head);
+ll_element **ll_getPrevPtr(int index, ll_head *head);
+void ll_destroy(ll_head *head);
+int ll_addElement(void *element, int index, ll_head *head);
+int ll_removeElement(int index, ll_head *head);
+
 //creates a new linked list in memory; make sure to free before termination
-ll_head *ll_create() {
+ll_head *ll_create(void (*deallocFn)(void *)) {
 	ll_head *list = malloc(sizeof(ll_head));
-	list.size = 0;
-	list.first = 0;
-	list.last = 0;
+	list->size = 0;
+	list->first = NULL;
+	list->last = NULL;				//TODO: use, implement, or remove
+	list->dealloc = deallocFn;
 	return list;
 }
 
+//returns pointer to numbered index, or null if invalid
 ll_element *ll_getIndex(int index, ll_head *head) {
 	if (index < 0 || index > head->size - 1) {
 		return 0;
@@ -37,56 +49,75 @@ ll_element *ll_getIndex(int index, ll_head *head) {
 	return elementptr;
 }
 
+//returns pointer of previous element (or head)
 ll_element **ll_getPrevPtr(int index, ll_head *head) {
 	if (index < 0 || index > head->size) {
 		return 0;
 	}
 	ll_element **elementptr = &(head->first);
-	for (int i = 0; i =< index; i++) {
+	for (int i = 0; i <= index; i++) {
 		elementptr = &((**elementptr).next);		//can be -> ?
 	}
 	return elementptr;
 }
 
+//destroys the list; deallocates all memory used
 void ll_destroy(ll_head *head) {
-	while(size > 0) {
+	while(head->size > 0) {
 		ll_removeElement(0, head);
 	}
-	free(*head);
+	free(head);
 }
 
-int ll_addElement(void *element, ll_head *head) {
-	return ll_addElement(element, 0, head);
-}
-
+//creates new entry from pointer and adds to list
 int ll_addElement(void *element, int index, ll_head *head) {
 	ll_element **prevptr = ll_getPrevPtr(index, head);
 	if (prevptr == 0) {
 		return 1;
 	}
-	ll_element *nextptr = *prevptr != 0 ? (**prevptr) : 0;
+	ll_element *nextptr = *prevptr != 0 ? (*prevptr)->next : NULL;
 
 	ll_element *newElement = malloc(sizeof(ll_element));
 	newElement->next = nextptr;
-	newElement->value = value;
+	newElement->value = element;
 	*prevptr = newElement;
 
 	head->size--;
 	return 0;
 }
 
-//TODO: deallocate element contents as well; how to do with nested lists
-//solution: supply a pointer to a destructor function that takes a void * pointing to the relevant object and destroys it as necessary; add to the whole list
+//removes entry from list; deallocates memory
 int ll_removeElement(int index, ll_head *head) {
 	ll_element **prevptr = ll_getPrevPtr(index, head);
-	if (prevptr == 0) {
+	if (prevptr == NULL) {
 		return 1;
 	}
-	ll_element *nextptr = *prevptr != 0 ? (**prevptr).next : 0;
-	free (**prevptr)
+	ll_element *nextptr = *prevptr != 0 ? (**prevptr).next : NULL;
+	ll_element *delptr = &(**prevptr);
+	head->dealloc(delptr->value);
+	free (delptr);
 	*prevptr = nextptr;
 	head->size--;
 	return 0;
+}
+
+void *getElement(int index, ll_head *head) {
+	return ll_getIndex(index, head)->value;
+}
+
+//destroys a linked list stored in another data structure
+void llRecDestructor(void *target) {
+	ll_destroy((ll_head *) target);
+}
+
+//destroys a basic object (struct, primitive, etc) stored in a data structure
+void basicDestructor(void *target) {
+	free(target);
+}
+
+//used when a destructor is needed but the contents do not need to be destroyed (are in stack, destroyed elsewhere, etc)
+void stubDestructor(void *target) {
+	//do nothing
 }
 
 
@@ -109,6 +140,19 @@ int main(int argc, char *argv[]) {
 	//bubbleSortGen(list, 5, (int (*)(void *, void *))compPtr);
 	//printList(list);
 	//printf("\n");
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	int e = 5;
+
+	ll_head *head = ll_create(&stubDestructor);
+	ll_addElement((void *)&a, 0, head);
+	ll_addElement((void *)&b, 0, head);
+	ll_addElement((void *)&c, 0, head);
+	ll_addElement((void *)&d, 0, head);
+	ll_addElement((void *)&e, 0, head);
+
 	return 0;
 }
 
@@ -147,10 +191,4 @@ void swap(void **a, void **b) {
 	temp = *a;
 	*a = *b;
 	*b = temp;
-}
-
-void printList(void *list[]) {
-	for (int i = 0; i < 5; i++) {
-		printf("%d, ", (*(struct blah *)list[i]).val);
-	}
 }
